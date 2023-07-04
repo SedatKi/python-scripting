59da4c8ed834936b2c6c46a113fa0103b482e7d5
SedatKi <sedatkirgiz11@gmail.com>
2023-07-04 00:49:18 +0000

    Task 1

---
+++ class_2/task_1.py
@@ -0,0 +1,8 @@
+string = "The cat is black, and it likes to sleep on the mat. Its name is Max."
+string_length = (len(string))
+if string_length < 100:
+    word1 = string[:3]
+if (len(word1)) < 5 and (len(word1)) > 2:
+    print('Success!')
+else:
+    print('Your word does not meet the requirement')
