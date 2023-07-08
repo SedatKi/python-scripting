d8e7546655bada93afb19f36bf28ca00875189c7
SedatKi <sedatkirgiz11@gmail.com>
2023-07-08 03:13:56 +0000

    Collection data types

---
+++ homework_3/data_structure.py
@@ -0,0 +1,2 @@
+user1 = {'Name': 'Kumo', 'Age': 23, 'Salary': 15000, 'Assets': ['car', 'laptop', 'tv', 'monitor']}
+user2 = {'Name': 'Mike', 'Age': 20, 'Salary': 10000, 'Assets': ['car', 'PC', 'smartwatch', 'boat']}
